d4a4111e084cf947ffc7867a584b007e0c515829
Flavio POGGIOLI <flavio.poggioli@etu.mines-ales.fr>
2025-02-07 14:16:41 +0100

    test gh action

---
+++ tests/test_app.py
@@ -0,0 +1,34 @@
+import unittest
+from app import app, favorites
+
+class CryptoTrackerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_get_crypto_price(self):
+        result = self.app.post('/get_crypto_price', data={'crypto': 'btcusd'})
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('btcusd', result.json)
+
+    def test_add_favorite(self):
+        global favorites  # Accéder à la variable globale
+        favorites.clear()  # Réinitialiser les favoris pour le test
+        result = self.app.post('/add_favorite', data={'crypto': 'btcusd'})
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('btcusd', result.json['favorites'])
+
+    def test_get_favorites(self):
+        global favorites  # Accéder à la variable globale
+        favorites.append('btcusd')  # Ajouter un favori pour le test
+        result = self.app.get('/get_favorites')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('btcusd', result.json['favorites'])
+
+if __name__ == '__main__':
+    unittest.main()
