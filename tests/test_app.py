02a9bf27d1d0315a44b04ef402492d0cdf03657f
Flavio POGGIOLI <flavio.poggioli@etu.mines-ales.fr>
2025-02-07 16:12:40 +0100

    adding caca

--- tests/test_app.py
+++ tests/test_app.py
@@ -28,7 +28,7 @@ class CryptoTrackerTestCase(unittest.TestCase):
         global favorites  # Accéder à la variable globale
         favorites.append('btcusd')  # Ajouter un favori pour le test
         result = self.app.get('/get_favorites')
-        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.status_code, 502)
         self.assertIn('btcusd', result.json['favorites'])
 
     def test_add_duplicate_favorite(self):
@@ -36,7 +36,7 @@ class CryptoTrackerTestCase(unittest.TestCase):
         favorites.clear()  # Réinitialiser les favoris pour le test
         self.app.post('/add_favorite', data={'crypto': 'btcusd'})
         result = self.app.post('/add_favorite', data={'crypto': 'btcusd'})
-        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.status_code, 404)
         self.assertEqual(favorites.count('btcusd'), 1)
 
 if __name__ == '__main__':
